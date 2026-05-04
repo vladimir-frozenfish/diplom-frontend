c432cc1ef9533893f7de1dd9f5686c9ed30beb08
VNKirichenko <VNKirichenko@inno.tech>
2026-05-04 15:01:22 +0300

    Task-001 - начальная страница                 - добавлен bg, header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
-export default function App() {
+import { NavLink } from "react-router"
+import Index from "./components/index/Index"
+import styles from './App.module.css'
 
+export default function App() {
   return (
-    <div>Кинозал</div>
-  )
-}
+    <div className={styles.app}>
+      <div className={styles.app_container}>
+        <div className={styles.app_header}>
+          <NavLink to='/'><div className={styles.app_logo}>ИДЁМ<span>В</span>КИНО</div></NavLink>
+          <NavLink to='/admin'><div className={styles.app_signin}>ВОЙТИ</div></NavLink>
+        </div>
+        
+        <Index />
+      </div>
+    </div>
+    )
+  }
