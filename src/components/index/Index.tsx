fb7cc6b2836fa5770cb0d0ad0f6e12464a460bff
VNKirichenko <VNKirichenko@inno.tech>
2026-05-04 17:23:26 +0300

    Task-001 - получение allData

--- src/components/index/Index.tsx
+++ src/components/index/Index.tsx
@@ -1,7 +1,31 @@
-import styles from './Index.module.css'
+import { useState, useEffect } from 'react'
+import { getResponse } from '../../utils/response'
+import type { AllDataType } from '../../types/types.ts'
+// import styles from './Index.module.css'
 
 export default function Index() {
+  const [allData, setAllData] = useState<AllDataType | null>(null)
+  const [isErrorResponse, setIsErrorResponse] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getResponse('/alldata')
+        setAllData(await response.json())
+        setIsLoading(false)
+      } catch (e) {
+        console.error(e)
+        setIsErrorResponse(true)
+      }
+    })()
+  }, [])
+
   return (
-      <div className={styles.index}>Кинотеатр</div>
+    <>
+      {isErrorResponse ? <div>Ошибка получения данных.</div> :
+        isLoading ? <div>Загрузка...</div> : <div>{allData ? Object.values(allData.result.films).length : 'Не найдено'}</div>
+      }
+    </>
   )
 }
--- src/enum/enum.ts
+++ src/enum/enum.ts
@@ -1,1 +1,2 @@
 export const basePath = '/diplom-frontend'
+export const apiUri = 'https://shfe-diplom.neto-server.ru'
