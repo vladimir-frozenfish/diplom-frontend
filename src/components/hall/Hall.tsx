64c75ffdf3c6ce954731d7bcf809c7326ecc1726
VNKirichenko <VNKirichenko@inno.tech>
2026-05-07 09:22:46 +0300

    Task-002 - компонент Hall - выбор мест на схеме зала

--- src/components/hall/Hall.tsx
+++ src/components/hall/Hall.tsx
@@ -18,19 +18,21 @@ interface SeatProps {
   seatIndex: number
 }
 
+function isTicketSelected(tickets: [number, number][], rowIndex: number, seatIndex: number) {
+  return tickets.some(ticket => ticket[0] === rowIndex && ticket[1] === seatIndex)
+}
+
 export default function Hall({selectedDate, selectedSeance, selectedFilm, selectedHall}: HallProps) {
   const [isLoading, setIsLoading] = useState(true)
   const [isErrorResponse, setIsErrorResponse] = useState(false)
   const [hallConfig, setHallConfig] = useState<DateHallConfig | null>(null)
+  const [tickets, setTickets] = useState<[number, number][]>([])
 
   useEffect(() => {
     (async () => {
       try {
         const response = await getResponse(`/hallconfig?seanceId=${selectedSeance?.id}&date=${selectedDate.toISOString().split('T')[0]}`)
-        let temp = await response.json()
-        console.log(temp)
-        // setHallConfig(await response.json())
-        setHallConfig(temp)
+        setHallConfig(await response.json())
         setIsLoading(false)
       } catch (e) {
         console.error(e)
@@ -47,14 +49,29 @@ export default function Hall({selectedDate, selectedSeance, selectedFilm, select
     return (<div>Загрузка...</div>)
   }  
 
+  function onClickSeat({seat, rowIndex, seatIndex}: SeatProps) {
+    if (seat === 'taken' || seat === 'disabled') {
+      return
+    }
+
+    setTickets(prevTickets => {
+      if (isTicketSelected(prevTickets, rowIndex, seatIndex)) {
+        return prevTickets.filter(ticket => !(ticket[0] === rowIndex && ticket[1] === seatIndex))
+      } else {
+        return [...prevTickets, [rowIndex, seatIndex]]
+      }
+    })
+  }
+
   function Seat({seat, rowIndex, seatIndex}: SeatProps) {
     let seatStyle = styles.hall_seat
     if (seat == 'vip') seatStyle += ' ' + styles.hall_seat_vip
     if (seat == 'disabled') seatStyle += ' ' + styles.hall_seat_disabled
     if (seat == 'taken') seatStyle += ' ' + styles.hall_seat_taken
+    if (isTicketSelected(tickets, rowIndex, seatIndex)) seatStyle += ' ' + styles.hall_seat_selected
     
     return (
-      <div className={seatStyle}></div>
+      <div className={seatStyle} onClick={() => onClickSeat({seat, rowIndex, seatIndex})}></div>
     )
   }
 
