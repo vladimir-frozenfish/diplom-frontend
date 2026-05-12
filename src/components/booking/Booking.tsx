90330ac173a863aeeac2761e566a8d2e04949c32
Vladimir <91720790+vladimir-frozenfish@users.noreply.github.com>
2026-05-12 10:06:39 +0300

    Task-004 - Исправление запроса на бронирование билетов и показ qr-кода

--- src/components/booking/Booking.tsx
+++ src/components/booking/Booking.tsx
@@ -1,4 +1,6 @@
-import type { FilmType, HallType, SeanceWithHallType, TicketsType } from '../../types/types.ts'
+import { useState } from 'react'
+import type { FilmType, HallType, SeanceWithHallType, TicketBookingType } from '../../types/types.ts'
+import { QRCode } from 'react-qr-code'
 import styles from './Booking.module.css'
 
 interface BookingProps {
@@ -6,14 +8,28 @@ interface BookingProps {
   selectedSeance: SeanceWithHallType | null
   selectedFilm: FilmType | null
   selectedHall: HallType | null
-  tickets: TicketsType
+  ticketsBooking: TicketBookingType[] | null
 }
 
 
-export default function Booking({selectedDate, selectedSeance, selectedFilm, selectedHall, tickets}: BookingProps) {
-  function ticketPrice() {
-    return  tickets.reduce((total, ticket) => {return total + ticket[2]}, 0)
+export default function Booking({selectedDate, selectedSeance, selectedFilm, selectedHall, ticketsBooking}: BookingProps) {
+  const [isShowQrCode, setIsShowQrCode] = useState(false)
+
+  let seats = ''
+  let price = 0
+  if (ticketsBooking) {
+    for (const ticket of ticketsBooking) {
+      seats += `Ряд - ${ticket.ticket_row} место - ${ticket.ticket_place}; `
+      price += ticket.ticket_price
+    }    
   }
+  seats = seats.trim()
+
+  const date = selectedDate.toLocaleDateString('ru-Ru')
+  const time = selectedSeance?.seance_time
+  const filmName = selectedFilm?.film_name
+  const hall = selectedHall?.hall_name ? selectedHall?.hall_name.charAt(0).toUpperCase() + selectedHall?.hall_name.slice(1) : '-'
+  const qrCodeString = `${date}, ${time}, ${filmName}, ${hall}, ${seats}, Цена - ${price}, Билет(ы) действителен строго на свой сеанс`
 
   return (
       <div>
@@ -24,20 +40,36 @@ export default function Booking({selectedDate, selectedSeance, selectedFilm, sel
 
         <div className={styles.booking}>
           <div className={styles.booking_info}>
-            <div>На фильм: <span>{selectedFilm?.film_name}</span></div>
-            <div>Места: {tickets.map((ticket, index) => <span key={index}>Ряд - {ticket[0] + 1} место - {ticket[1] + 1}; </span>)}</div>
-            <div>В зале: <span>{selectedHall?.hall_name ? selectedHall?.hall_name.charAt(0).toUpperCase() + selectedHall?.hall_name.slice(1) : '-'}</span></div>
-            <div>Дата: <span>{selectedDate.toLocaleDateString('ru-Ru')}</span></div>
-            <div>Начало сеанса: <span>{selectedSeance?.seance_time}</span></div>
-            <div>Стоимость: <span>{ticketPrice()}</span></div>
+            <div>На фильм: <span>{filmName}</span></div>
+            <div>Места: <span>{seats}</span></div>
+            <div>В зале: <span>{hall}</span></div>
+            <div>Дата: <span>{date}</span></div>
+            <div>Начало сеанса: <span>{time}</span></div>
+            <div>Стоимость: <span>{price}</span></div>
           </div>
           
-          <div className={styles.booking_button}>ПОЛУЧИТЬ КОД БРОНИРОВАНИЯ</div>
+          {!isShowQrCode 
+            ?
+            <>
+              <div className={styles.booking_button} onClick={() => setIsShowQrCode(true)}>ПОЛУЧИТЬ КОД БРОНИРОВАНИЯ</div>
+            
+              <div className={styles.booking_footer_info}>
+                <div>После оплаты билет будет доступен в этом окне, а также придёт вам на почту. Покажите QR-код нашему контроллёру у входа в зал.</div>
+                <div>Приятного просмотра!</div>          
+              </div>
+            </>
+            :
+            <>
+              <div className={styles.booking_qrcode_container}>
+                <QRCode value={qrCodeString} />
+              </div>
+              <div className={styles.booking_footer_info}>
+                <div>Покажите QR-код нашему контроллеру для подтверждения бронирования.</div>
+                <div>Приятного просмотра!</div>          
+              </div>
+            </> 
+          }
           
-          <div className={styles.booking_footer_info}>
-            <div>После оплаты билет будет доступен в этом окне, а также придёт вам на почту. Покажите QR-код нашему контроллёру у входа в зал.</div>
-            <div>Приятного просмотра!</div>          
-          </div>
         </div>
 
         <div className={styles.booking_ticket_img + ' ' + styles.booking_ticket_img_scale}></div>
