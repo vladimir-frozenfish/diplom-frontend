d4e480b150a6aaec9515b10a687f08931ace60bd
VNKirichenko <VNKirichenko@inno.tech>
2026-05-13 09:51:44 +0300

    Task-005 - получить информацию alldata в админской части

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -1,10 +1,45 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { NavLink } from "react-router"
+import { getResponse } from '../../utils/response'
+import type { AllDataType } from '../../types/types.ts'
 import Auth from "../auth/Auth"
 import styles from './Admin.module.css'
 
 export default function Admin() {
   const [isAuth, setIsAuth] = useState(false)
+  const [allData, setAllData] = useState<AllDataType | null>(null)
+  const [isErrorResponse, setIsErrorResponse] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)  
+
+  useEffect(() => {
+    (async () => {
+      if (isAuth) {
+        setIsLoading(true)
+        try {
+          const response = await getResponse('/alldata')
+          setAllData(await response.json())
+          setIsLoading(false)
+        } catch (e) {
+          console.error(e)
+          setIsErrorResponse(true)
+        }
+      }
+    })()
+  }, [isAuth])  
+
+  if (isErrorResponse) {
+    return (
+      <div className={styles.admin}>
+        <div className={styles.admin_container}><div>Ошибка получения данных.</div></div>
+      </div>
+  )}
+
+  if (isLoading) {
+    return (
+      <div className={styles.admin}>
+        <div className={styles.admin_container}><div>Загрузка...</div></div>
+      </div>
+  )}
 
   return (
       <div className={styles.admin}>
