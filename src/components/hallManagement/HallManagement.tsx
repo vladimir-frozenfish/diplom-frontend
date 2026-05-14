ea8770849030b17295658fb1e854707b21ea7df7
VNKirichenko <VNKirichenko@inno.tech>
2026-05-14 11:12:28 +0300

    Task-005 - управление залами - добавлениа форма добавления зала

--- src/components/hallManagement/HallManagement.tsx
+++ src/components/hallManagement/HallManagement.tsx
@@ -1,9 +1,11 @@
-// import { useState } from 'react'
+import { useState } from 'react'
+import type { FormEvent } from 'react'
 import type { HallType } from '../../types/types.ts'
 import { basePath } from '../../enum/enum.ts'
 // import { QRCode } from 'react-qr-code'
 import Button from '../../utils/button/Button.tsx'
 import styles from './HallManagement.module.css'
+import stylesAdminForm from  '../../css/FormAdmin.module.css'
 
 interface HallManagementProps {
   halls: HallType[] | undefined
@@ -15,6 +17,31 @@ interface HallManagementProps {
 }
 
 export default function HallManagement({halls}: HallManagementProps) {
+  const [isAddHall, setIsAddHall] = useState(false)
+
+  async function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    setIsAddHall(false)
+    // const form = new FormData(e.currentTarget)
+
+    // try {
+    //   const response = await getResponseFromForm('/login', 'POST', form)
+    //   const data = await response.json()
+
+    //   if (data.success && data.result == 'Авторизация пройдена успешно!') {
+    //     setIsAuth(true)
+    //   } else {
+    //     setIsAuthError(true)
+    //   }
+    // } catch(e) {
+    //   console.error(e)
+    // }    
+  }
+
+  function onReset() {
+    setIsAddHall(false)
+  }
 
   return (
       <div>
@@ -32,8 +59,27 @@ export default function HallManagement({halls}: HallManagementProps) {
           ) : (<li>Залов не найдено</li>)}
         </ul>
         <div className={styles.hall_management_buttons}>
-          <Button text='СОЗДАТЬ ЗАЛ' onClick={() => {}} />
+          <Button text='СОЗДАТЬ ЗАЛ' onClick={() => {setIsAddHall(true)}} />
         </div>
+        
+        {isAddHall && 
+          <div className={stylesAdminForm.admin_form_modal}>
+            <div className={stylesAdminForm.admin_form_container}>
+              <div className={stylesAdminForm.admin_form_header}>ДОБАВЛЕНИЕ ЗАЛА</div>
+              <form className={stylesAdminForm.admin_form} onSubmit={onSubmit} onReset={onReset}>
+                <div  className={stylesAdminForm.admin_form_fields}>
+                  <div className={stylesAdminForm.admin_form_description}>Название зала</div>
+                  <input placeholder='Зал 1' name='hallName' className={stylesAdminForm.admin_form_input}/>
+                </div>
+
+                <div className={stylesAdminForm.admin_form_buttons}>
+                  <button type="submit" className={stylesAdminForm.admin_form_button + ' ' + stylesAdminForm.admin_form_button_submit}>ДОБАВИТЬ ЗАЛ</button>
+                  <button type="reset" className={stylesAdminForm.admin_form_button}>ОТМЕНИТЬ</button>
+                </div>                
+              </form>
+            </div>
+          </div>
+        }
       </div>
   )
 }
