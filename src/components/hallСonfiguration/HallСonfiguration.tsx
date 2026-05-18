55d0b8bd504f05a096da98046ff1dea40f5861ee
VNKirichenko <VNKirichenko@inno.tech>
2026-05-18 11:01:29 +0300

    Task-006 - отображение блока количества рядов и кресел в зале

--- src/components/hallСonfiguration/HallСonfiguration.tsx
+++ src/components/hallСonfiguration/HallСonfiguration.tsx
@@ -17,9 +17,11 @@ export default function HallСonfiguration({halls, setIsUpdateData}: HallHallСo
   // const [isAddHall, setIsAddHall] = useState(false)
   // const [deleteHall, setDeleteAddHall] = useState<HallType | null>(null)
   const [currentHall, setCurrentHall] = useState<HallType | null>(null)
+  const [rowsHall, setRowsHall] = useState(0)
+  const [placesHall, setPlacesHall] = useState(0)
   const [isLoading, setIsLoading] = useState(false)
-  const [isAddHallError, setIsAddHallError] = useState(false)
-  const [isDeleteHalllError, setIsDeleteHallError] = useState(false)
+  // const [isAddHallError, setIsAddHallError] = useState(false)
+  // const [isDeleteHalllError, setIsDeleteHallError] = useState(false)
 
   // async function onSubmit(e: FormEvent<HTMLFormElement>) {
   //   e.preventDefault()
@@ -51,6 +53,13 @@ export default function HallСonfiguration({halls, setIsUpdateData}: HallHallСo
   //   setIsAddHallError(false)
   // }
 
+  function onClickHall(hall: HallType) {
+    setCurrentHall(hall)
+    setRowsHall(hall.hall_rows)
+    setPlacesHall(hall.hall_places)
+    // console.log(hall)
+  }
+
   return (
       <div>
         <div>Выберите зал для конфигурации:</div>
@@ -60,14 +69,33 @@ export default function HallСonfiguration({halls, setIsUpdateData}: HallHallСo
               <div 
                 className={currentHall?.id === hall.id ? styles.hall_configuration_nav_button + ' ' + styles.hall_configuration_nav_button_current : styles.hall_configuration_nav_button} 
                 key={index} 
-                onClick={() => setCurrentHall(hall)}
+                onClick={() => onClickHall(hall)}
               >
                 {hall.hall_name}
               </div>
             ))
           ) : (<div>Залов не найдено</div>)}
         </div>
-        
+
+        <div className={styles.hall_configuration_rows_container}>
+          <div>Укажите количество рядов и максимальное количество кресел в ряду:</div>
+          <div className={styles.hall_configuration_rows_container_inputs}>
+            <div className={styles.hall_configuration_rows_container_input_filed}>
+              <div className={styles.hall_configuration_rows_container_input_title}>Рядов, шт</div>
+              <input className={styles.hall_configuration_rows_container_input} type='number' value={rowsHall} onChange={(e) => setRowsHall(+e.currentTarget.value)}></input>
+            </div>
+            <div>
+              <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8.74512 8.82422H6.71484L4.33301 5.45801L1.97754 8.82422H0L3.41895 4.39453L0.158203 0H2.11816L4.43848 3.39258L6.71484 0H8.60449L5.29102 4.42969L8.74512 8.82422Z" fill="#848484"/>
+              </svg>
+            </div>
+            <div className={styles.hall_configuration_rows_container_input_filed}>
+              <div className={styles.hall_configuration_rows_container_input_title}>Мест, шт</div>
+              <input className={styles.hall_configuration_rows_container_input} type='number' value={placesHall} onChange={(e) => setPlacesHall(+e.currentTarget.value)}></input>
+            </div>            
+          </div>
+        </div>
+
         {/* <div className={styles.hall_management_buttons}>
           <Button text='СОЗДАТЬ ЗАЛ' onClick={() => {setIsAddHall(true)}} />
         </div> */}
