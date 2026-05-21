83dc739d1a84213d8f7bdd6ccf7b340a828594ce
Vladimir <91720790+vladimir-frozenfish@users.noreply.github.com>
2026-05-21 13:12:31 +0300

    Review-001 - Ревью

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,3 +1,4 @@
+import { areDatesEqual } from '../../utils/utils'
 import styles from './Calendar.module.css'
 
 interface CalendarProps {
@@ -10,14 +11,6 @@ interface DayProps {
 }
 
 const days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
-
-function areDatesEqual(dateOne: Date, dateTwo: Date): boolean {
-  return (
-    dateOne.getFullYear() === dateTwo.getFullYear() &&
-    dateOne.getMonth() === dateTwo.getMonth() &&
-    dateOne.getDate() === dateTwo.getDate()
-  )
-}
 
 export default function Calendar({selectedDate, onSelectDate}: CalendarProps) {
   const currentDate = new Date()
@@ -55,7 +48,7 @@ export default function Calendar({selectedDate, onSelectDate}: CalendarProps) {
   return (
     <div className={styles.calendar}>
         {dates.map((date, index) => <Day key={index} date={date} />)}
-        <div className={styles.calendar_day}>{'>'}</div>
+        <div className={styles.calendar_day + ' ' + styles.calendar_day_more}>{'>'}</div>
       </div>
   )
 }
--- src/components/film/Film.tsx
+++ src/components/film/Film.tsx
@@ -9,13 +9,20 @@ interface FilmProps {
   setSelectedSeance: (seance: SeanceWithHallType) => void
   setSelectedFilm: (film: FilmType) => void
   setSelectedHall: (hall: HallType | null) => void
+  selectedDate: Date
 }
 
 interface HallProps {
   seances: SeanceWithHallType[]
 }
 
-export default function Film({film, seances, halls, setClientPage, setSelectedSeance, setSelectedFilm, setSelectedHall}: FilmProps) {
+function isSeanceActive(seance: SeanceType, selectedDate: Date) {
+  const timeSeance = seance.seance_time.split(':')
+  const seanseDate = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate(), +timeSeance[0], +timeSeance[1])
+  return seanseDate > (new Date())
+}
+
+export default function Film({film, seances, halls, setClientPage, setSelectedSeance, setSelectedFilm, setSelectedHall, selectedDate}: FilmProps) {
   const groupSeancesOfHalls: Record<number, SeanceWithHallType[]> = {}
   for (const seance of seances) {
     if (!groupSeancesOfHalls[seance.seance_hallid]) groupSeancesOfHalls[seance.seance_hallid] = []
@@ -36,7 +43,7 @@ export default function Film({film, seances, halls, setClientPage, setSelectedSe
       <div>
         <div className={styles.film_hallname}>{seances[0].seance_hallname ? seances[0].seance_hallname.charAt(0).toUpperCase() + seances[0].seance_hallname.slice(1) : '-'}</div>
         <div className={styles.film_seances}>
-          {seances.map((seance, index) => <div onClick={() => onClickSeance(seance)} key={index} className={styles.film_seance}>{seance.seance_time}</div>)}
+          {seances.sort((a, b) => a.seance_time.localeCompare(b.seance_time)).map((seance, index) => <div onClick={() => onClickSeance(seance)} key={index} className={isSeanceActive(seance, selectedDate) ? styles.film_seance : styles.film_seance + ' ' + styles.film_seance_not_active}>{seance.seance_time}</div>)}
         </div>
       </div>
     )
@@ -46,10 +53,11 @@ export default function Film({film, seances, halls, setClientPage, setSelectedSe
     <div className={styles.film}>
       <div className={styles.film_poster_and_description}>
         <img src={film.film_poster} className={styles.film_poster}/>
-        <div>
+        <div className={styles.film_description_container}>
           <div className={styles.film_name}>{film.film_name}</div>
           <div className={styles.film_description}>{film.film_description}</div>
           <div className={styles.film_duration}>{film.film_duration} минут {film.film_origin}</div>
+          <div className={styles.film_rectangle}></div>
         </div>
       </div>
 
