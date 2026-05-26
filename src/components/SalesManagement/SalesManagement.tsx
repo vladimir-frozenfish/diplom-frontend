83221a39284e8b3175a66064c3ee3a325e79e344
VNKirichenko <VNKirichenko@inno.tech>
2026-05-26 13:16:30 +0300

    Task-009 - Административная часть - раздел Открыть продажи:   - добавить компонент SalesManagement (пока без функциональности)   - отобразить залы   - сверстать необходимые элементы

---
+++ src/components/SalesManagement/SalesManagement.tsx
@@ -0,0 +1,130 @@
+import { useState } from 'react'
+import type { FormEvent, Dispatch, SetStateAction } from 'react'
+import type { HallType } from '../../types/types.ts'
+import { getResponseFromForm } from '../../utils/response.ts'
+import LoadingModal from '../../utils/loadingModal/LoadingModal.tsx'
+import Button from '../../utils/button/Button.tsx'
+import styles from './SalesManagement.module.css'
+import stylesAdminForm from  '../../css/FormAdmin.module.css'
+
+interface SalesManagementProps {
+  halls: HallType[] | undefined
+  setIsUpdateData: Dispatch<SetStateAction<boolean>>
+}
+
+export default function SalesManagement({halls, setIsUpdateData}: SalesManagementProps) {
+  const [isConfirmSales, setIsConfirmSales] = useState(false)
+  const [currentHall, setCurrentHall] = useState<HallType | null>(null)
+  // const [priceStandart, setPriceStandart] = useState(0)
+  // const [priceVip, setPriceVip] = useState(0)
+  const [isLoading, setIsLoading] = useState(false)
+  // const [isСonfigurationPriceError, setIsСonfigurationPriceError] = useState(false)
+
+  function onClickHall(hall: HallType) {
+    setCurrentHall(hall)
+    console.log(hall.hall_open)
+    // setPriceStandart(hall.hall_sales_standart)
+    // setPriceVip(hall.hall_sales_vip)
+  }
+
+  // async function onConfirm(e: FormEvent<HTMLFormElement>) {
+  //   e.preventDefault()
+  //   setIsLoading(true)
+    
+  //   const form = new FormData()
+  //   form.set('priceStandart', String(priceStandart))
+  //   form.set('priceVip', String(priceVip))
+
+  //   try {
+  //     const response = await getResponseFromForm(`/price/${currentHall?.id}`, 'POST', form)
+  //     const data = await response.json()
+
+  //     if (data.success) {
+  //       setIsConfirmPrice(false)
+  //       setIsUpdateData((current) => !current)
+  //     } else {
+  //       setIsСonfigurationPriceError(true)
+  //     }
+      
+  //   } catch(e) {
+  //     console.error(e)
+  //   }
+    
+  //   setIsLoading(false)
+  // }
+
+  // function onResetConfirm() {
+  //   setIsConfirmPrice(false)
+  //   setIsСonfigurationPriceError(false)
+  // }
+
+  return (
+      <div>
+        <div>Выберите зал для открытия/закрытия продаж:</div>
+        <div className={styles.sales_configuration_nav}>
+          {halls && halls.length > 0 ? (
+            halls.map((hall, index) => (
+              <div 
+                className={currentHall?.id === hall.id ? styles.sales_configuration_nav_button + ' ' + styles.sales_configuration_nav_button_current : styles.sales_configuration_nav_button} 
+                key={index} 
+                onClick={() => onClickHall(hall)}
+              >
+                {hall.hall_name}
+              </div>
+            ))
+          ) : (<div>Залов не найдено</div>)}
+        </div>
+
+        <div className={styles.sales_configuration_title}>{!currentHall?.hall_open ? 'Всё готово к открытию' : 'Можно закрыть зал'}</div>
+
+        {/* <div className={styles.sales_configuration_container}>
+          <div>Установите цены для типов кресел:</div>
+            <div className={styles.sales_configuration_inputs}>
+              <div className={styles.sales_configuration_input_filed}>
+                <div className={styles.sales_configuration_input_title}>Цена, рублей</div>
+                <input className={styles.sales_configuration_input} type='number' value={priceStandart} onChange={(e) => setPriceStandart(+e.currentTarget.value)}></input>
+              </div>
+              <div className={styles.sales_configuration_legend}>
+                <div>за</div>
+                <div className={styles.sales_configuration_seat}></div> 
+                <div>обычные кресла</div>
+              </div>
+
+              <div className={styles.sales_configuration_input_filed}>
+                <div className={styles.sales_configuration_input_title}>Цена, рублей</div>
+                <input className={styles.sales_configuration_input + ' ' + styles.sales_configuration_input_vip} type='number' value={priceVip} onChange={(e) => setPriceVip(+e.currentTarget.value)}></input>
+              </div>
+              <div className={styles.sales_configuration_legend}>
+                <div>за</div>
+                <div className={styles.sales_configuration_seat + ' ' + styles.sales_configuration_seat_vip}></div>
+                <div>VIP кресла</div>
+              </div>
+            </div>
+        </div> */}
+
+        <div className={styles.sales_configuration_buttons}>
+          <Button text={!currentHall?.hall_open ? 'ОТКРЫТЬ ПРОДАЖУ БИЛЕТОВ' : 'ЗАКРЫТЬ ПРОДАЖУ БИЛЕТОВ'} onClick={() => {if (currentHall) setIsConfirmSales(true)}} />
+        </div>
+
+        {/* {isConfirmPrice && 
+          <div className={stylesAdminForm.admin_form_modal}>
+            <div className={stylesAdminForm.admin_form_container}>
+              <div className={stylesAdminForm.admin_form_header}>КОНФИГУРАЦИЯ ЦЕН</div>
+              <form className={stylesAdminForm.admin_form} onSubmit={onConfirm} onReset={onResetConfirm}>
+                <div className={stylesAdminForm.admin_form_caption}>Сохранить цены зала - <span>{currentHall?.hall_name}?</span></div>
+
+                <div className={stylesAdminForm.admin_form_buttons}>
+                  <button type="submit" className={stylesAdminForm.admin_form_button + ' ' + stylesAdminForm.admin_form_button_submit}>СОХРАНИТЬ</button>
+                  <button type="reset" className={stylesAdminForm.admin_form_button}>ОТМЕНИТЬ</button>
+                </div>
+
+                {isСonfigurationPriceError && <div className={stylesAdminForm.admin_form_error}>Не удалось сохранить цены зала.</div>}
+              </form>
+            </div>
+          </div>
+        }             */}
+
+        {isLoading && <LoadingModal />}
+      </div>
+  )
+}
